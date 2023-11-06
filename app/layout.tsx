99b6299e0a931ca7cba1b145be26b11ebe806500
wellwich <mail@wellwich.com>
2023-11-07 08:47:35 +0900

    well

--- app/layout.tsx
+++ app/layout.tsx
@@ -3,8 +3,8 @@ import './globals.css'
 
 export const metadata: Metadata = {
   title: {
-    default: '無料ブラウザゲームサイト @games.wellwich.com ',
-    template: `%s | 無料ブラウザゲームサイト @games.wellwich.com`
+    default: '無料ブラウザゲームサイト＠うぇる',
+    template: `%s | 無料ブラウザゲームサイト＠うぇる`
   },
   description: '無料でブラウザで遊べるゲームサイトです。',
 }
@@ -21,7 +21,7 @@ export default function RootLayout({
         <header>
           <nav className="flex items-center justify-between flex-wrap bg-gray-500 p-6">
             <div className="flex items-center flex-shrink-0 text-white mr-6">
-              <h1 className="font-semibold text-xl tracking-tight">無料ブラウザゲームサイト @games.wellwich.com</h1>
+              <h1 className="font-semibold text-xl tracking-tight">無料ブラウザゲームサイト＠うぇる</h1>
             </div>
             <div className="w-full block flex-grow lg:flex lg:items-center lg:w-auto">
               <div className="text-sm lg:flex-grow">
