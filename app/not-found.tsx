d546789f0858a20c90337e37d9fdce765bb38b43
wellwich <mail@wellwich.com>
2024-05-04 12:30:03 +0900

    Create not-found.tsx

---
+++ app/not-found.tsx
@@ -0,0 +1,10 @@
+export const runtime = "edge"
+
+export default function NotFound() {
+    return (
+        <>
+            <h1>404 Not Found</h1>
+            <p>ページが見つかりませんでした。</p>
+        </>
+    )
+}
